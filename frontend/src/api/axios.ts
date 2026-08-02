6a484a45ec94d5b9ac58d6b700a821b7470d49c4
anuz2004 <anuzbk370@gmail.com>
2026-08-02 16:52:07 +0530

    Fix backend url change 2

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://growthos-backend-anuz2004.onrender.com",
+  baseURL: "https://growthos-backend-anuz2004.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
